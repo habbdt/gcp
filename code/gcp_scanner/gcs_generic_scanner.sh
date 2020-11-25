a7e0aa0dfd9e5e1aa24b521e9dd79ba319544a35
habbdt@gmail.com <habbdt@gmail.com>
2020-11-25 11:13:49 -0500

    check if an bucket is empty or not

--- code/gcp_scanner/gcs_generic_scanner.sh
+++ code/gcp_scanner/gcs_generic_scanner.sh
@@ -11,7 +11,7 @@ GCS_SCAN=$2
 if [ "$#" -ne 2 ]; then
   echo "Usage: gcs_generic_scanner.sh <search_phrase> <gcs_search>"
   echo "search phrase - common word in the gcp project naming pattern"
-  echo "GCS_SEARCH: BACKEND_BUCKETS"
+  echo "GCS_SEARCH: BACKEND_BUCKETS, EMPTY_BUCKET"
   exit
 fi
 
@@ -41,12 +41,48 @@ function BACKEND_BUCKETS() {
 }
 
 
+# check if a gcs bucket is empty or not
+
+function EMPTY_BUCKET() {
+    GCP_PROJECT=$(gcloud projects list --format="get(projectId)" \
+                  --filter=$SEARCH_PHRASE)
+
+    for proj in $GCP_PROJECT; do
+      API_STATUS="$(gcloud services list --enabled --filter=NAME~storage \
+        --project $proj)"
+      if [ -z "$API_STATUS" ]; then
+        :
+      else
+        BUCKET_LIST="$(gsutil ls -p $proj)"
+        if [ -z "$BUCKET_LIST" ]
+        then
+          :
+        else
+          for bucket in $BUCKET_LIST ; do
+            STAT_SZ="$(gsutil du -s $bucket | awk '{print $1}')"
+            zero=0
+            if [[ $STAT_SZ -eq $zero ]]
+            then
+              echo "[!] bucket $bucket in project $proj is EMPTY"
+            else
+              echo "[x] bucket $bucket in project $proj is non-empty"
+            fi
+          done
+        fi
+      fi
+    done
+}
+
 # execute function based on script argument
 
 case $GCS_SCAN in
 
   BACKEND_BUCKETS)
     BACKEND_BUCKETS
+    ;;
+
+  EMPTY_BUCKET)
+    EMPTY_BUCKET
     ;;
 
 esac
