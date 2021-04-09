a59e4e28a8b03c669a31de402a2f008e5068627b
habbdt@gmail.com <habbdt@gmail.com>
2021-04-09 10:37:50 -0400

    gclb port range scanner

--- code/gcp_scanner/gclb_portrange.sh
+++ code/gcp_scanner/gclb_portrange.sh
@@ -19,18 +19,25 @@ function gclb_portrange() {
                   --filter=$SEARCH_TERM)"
 
     for proj in $GCP_PROJECT; do
-      GCLB_NAME="$(gcloud compute forwarding-rules list --project $proj --format='get(NAME)')"
-      if [ -z "$GCLB_NAME" ]
+      API_STATUS="$(gcloud services list --enabled --filter=NAME~compute \
+                    --project $proj)"
+      if [ -z "$API_STATUS" ]
       then
-        echo "[!] NO GCLB CONFIGURED FOR PROJECT $proj"
+        :
       else
-        echo "proj, gclb_name, ip_address, kind, port_range"
-        for gclb in $GCLB_NAME; do
-          PORT_FORWARDING_RULE="$(gcloud compute forwarding-rules describe $gclb --global --project $proj\
-                                  --format=json | \
-                                  jq -r '[.name,.IPAddress,.kind,.portRange] | @csv')"
-          echo "$proj, $PORT_FORWARDING_RULE"
-        done
+        GCLB_NAME="$(gcloud compute forwarding-rules list --project $proj --format='get(NAME)')"
+        if [ -z "$GCLB_NAME" ]
+        then
+          echo "[!] NO GCLB CONFIGURED FOR PROJECT $proj"
+        else
+          echo "proj, gclb_name, ip_address, kind, port_range"
+          for gclb in $GCLB_NAME; do
+            PORT_FORWARDING_RULE="$(gcloud compute forwarding-rules describe $gclb --global --project $proj\
+                                    --format=json | \
+                                    jq -r '[.name,.IPAddress,.kind,.portRange] | @csv')"
+            echo "$proj, $PORT_FORWARDING_RULE"
+          done
+        fi
       fi
     done
 }
